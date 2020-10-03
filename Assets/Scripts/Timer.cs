43dc34ef06f363a2705f1335baf79033e040bee9
Isaiah Mann <isaiahbmann@gmail.com>
2020-10-03 11:25:35 -0700

    Implement game timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField]
+    GameState state;
+
+    [SerializeField]
+    GameConfig config;
+
+    [SerializeField]
+    Text timerText;
+
+    Action onTimerEnd;
+    float maxTime;
+    float timeRemainingSeconds;
+
+    public void SubscribeToTimerEnd(Action handler)
+    {
+        onTimerEnd += handler;
+    }
+
+    public void RestartTimer()
+    {
+        timeRemainingSeconds = maxTime;
+    }
+
+    public void UpdateMaxTime(float maxTime)
+    {
+        this.maxTime = maxTime;
+    }
+
+    private void Awake()
+    {
+        // offset by one second, otherwise, starting time never appears:
+        maxTime = config.TimerStartSeconds + 1;
+        RestartTimer();
+        UpdateUI();
+        onTimerEnd = () => { };
+    }
+
+    private void Update()
+    {
+        if(state.Paused || Mathf.Approximately(timeRemainingSeconds, 0))
+        {
+            return;
+        }
+
+        UpdateUI();
+        timeRemainingSeconds -= Time.deltaTime;
+        timeRemainingSeconds = Mathf.Max(0, timeRemainingSeconds);
+        if(Mathf.Approximately(timeRemainingSeconds, 0))
+        {
+            onTimerEnd();
+        }
+    }
+
+    private void UpdateUI()
+    {
+        string minutes = Mathf.FloorToInt(timeRemainingSeconds / 60).ToString();
+        string seconds = (Mathf.FloorToInt(timeRemainingSeconds) % 60).ToString();
+
+        timerText.text = string.Format("{0}:{1}", PadZero(minutes, 2), PadZero(seconds, 2));
+    }
+
+    private string PadZero(string time, int targetLength)
+    {
+        if(time.Length < targetLength)
+        {
+            return string.Format("{0}{1}", new string('0', targetLength - time.Length), time);
+        }
+        return time;
+    }
+}
