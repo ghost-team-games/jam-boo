daabbbc11224b726a22af853a32484d336da0d20
mhollister92 <mhollister92@gmail.com>
2020-10-03 22:50:06 +0100

    Change Colors on Mouse Over
    - turns green when mouse over and clickable
    - turns red when mouse over and not clickable

--- Assets/Scripts/InteractableObject.cs
+++ Assets/Scripts/InteractableObject.cs
@@ -10,15 +10,21 @@ public class InteractableObject : MonoBehaviour
     public float cooldownTimer;
     public float animationLength;
     public float customFearAmount = 0;
+    public Material redMaterial;
+    public Material greenMaterial;
 
     private Animator animator;
     private bool cooldown;
+    private Renderer rend;
+    private Material baseMaterial;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
         cooldown = false;
+        rend = GetComponent<Renderer>();
+        baseMaterial = GetComponent<Renderer>().material;
     }
 
     // Update is called once per frame
@@ -43,6 +49,23 @@ public class InteractableObject : MonoBehaviour
                 fear.IncreaseFear();
             }
         }
+    }
+
+    private void OnMouseOver()
+    {
+        if(cooldown)
+        {
+            rend.material = redMaterial;
+        }
+        else
+        {
+            rend.material = greenMaterial;
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        rend.material = baseMaterial; 
     }
 
     private IEnumerator AnimationDelay(float animationLength)
