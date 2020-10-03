c7b3523768b24626ab130caaec2c9cfc0ae392e2
Isaiah Mann <isaiahbmann@gmail.com>
2020-10-03 13:30:42 -0700

    Make fear and timer get more difficult per level

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -4,9 +4,24 @@ public class GameState : MonoBehaviour
 {
     public bool Paused { get; private set; }
 
+    [SerializeField]
+    Timer timer;
+
+    [SerializeField]
+    FearMeter fear;
+
+    [SerializeField]
+    GameConfig config;
+
+    float timerMaxSeconds;
+    float generationsScared;
+
     private void Awake()
     {
         Paused = false;
+        timerMaxSeconds = config.TimerStartSeconds;
+        generationsScared = 0;
+        fear.SubscribeToFearAtMax(NextLevel);
     }
 
     public void Play()
@@ -18,5 +33,17 @@ public class GameState : MonoBehaviour
     {
         Paused = true;
 
+    }
+
+    public void NextLevel()
+    {
+        if(timerMaxSeconds > config.TimerMinSeconds)
+        {
+            timerMaxSeconds -= config.TimerDecrease;
+            timer.UpdateMaxTime(timerMaxSeconds);
+        }
+        timer.RestartTimer();
+        fear.IncreaseFearChallenge(config.FearMeterMaxIncrease, config.FearDecreaseIncrement);
+        generationsScared++;
     }
 }
