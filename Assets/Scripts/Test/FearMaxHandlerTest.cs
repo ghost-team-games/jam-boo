0d51d2392e0a313f219e0dd76de842d87f5319b0
Isaiah Mann <isaiahbmann@gmail.com>
2020-10-03 12:39:00 -0700

    Add fear meter

---
+++ Assets/Scripts/Test/FearMaxHandlerTest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class FearMaxHandlerTest : MonoBehaviour
+{
+    [SerializeField]
+    FearMeter fearMeter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fearMeter.SubscribeToFearAtMax(FearAtMax);
+    }
+
+    void FearAtMax()
+    {
+        Debug.Log("Fear at max");
+    }
+}
