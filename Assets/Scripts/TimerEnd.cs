e7112b3c879005e6a0f22be5976989c41b2835e6
mhollister92 <mhollister92@gmail.com>
2020-10-04 16:21:45 +0100

    Added end screen
    - loads on timer end
    - fades out main scene
    - fades in end scene
    - displays game score
    - displays highest score
    - lets player know if high score reached
    - replay button

---
+++ Assets/Scripts/TimerEnd.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TimerEnd : MonoBehaviour
+{
+    [SerializeField]
+    Timer timer;
+
+    [SerializeField]
+    MenuController menuController;
+
+    [SerializeField]
+    GameState state;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer.SubscribeToTimerEnd(OnEnd);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnEnd()
+    {
+        menuController.LoadScene(2);
+        PlayerPrefs.SetInt("Score", state.generationsScared);
+    }
+}
