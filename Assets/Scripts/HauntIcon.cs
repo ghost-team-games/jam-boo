50dcc4ba2c0124454411017ad2727e42136cb70a
mhollister92 <mhollister92@gmail.com>
2020-10-05 12:42:04 +0100

    Fixed cursor
    - made it so the haunt icon moves with the cursor instead of just pointing at the objects

--- Assets/Scripts/HauntIcon.cs
+++ Assets/Scripts/HauntIcon.cs
@@ -2,35 +2,58 @@
 
 public class HauntIcon : MonoBehaviour
 {
-    [SerializeField]
-    Sprite hauntIcon;
+    //[SerializeField]
+    //Sprite hauntIcon;
+
+   // [SerializeField]
+   // Sprite hauntDisabledIcon;
 
     [SerializeField]
-    Sprite hauntDisabledIcon;
+    Texture2D hauntCursor;
+
+    [SerializeField]
+    Texture2D noHauntCursor;
+
+    [SerializeField]
+    Texture2D normalCursor;
 
     SpriteRenderer icon;
 
+    private CursorMode cursorMode;
+    private Vector2 cursorPoint;
+
     private void Awake()
     {
-        icon = GetComponent<SpriteRenderer>();
-        icon.enabled = false;
+        //icon = GetComponent<SpriteRenderer>();
+        //icon.enabled = false;
+
+        cursorMode = CursorMode.Auto;
+        cursorPoint = Vector2.zero;
+        HideIcon();
+
     }
 
     public void ShowHauntEnabledIcon()
     {
-        icon.enabled = true;
-        icon.sprite = hauntIcon;
+        //icon.enabled = true;
+        //icon.sprite = hauntIcon;
+
+        Cursor.SetCursor(hauntCursor, cursorPoint, cursorMode);
     }
 
     public void ShowHauntDisabledIcon()
     {
-        icon.enabled = true;
-        icon.sprite = hauntDisabledIcon;
+        //icon.enabled = true;
+        //icon.sprite = hauntDisabledIcon;
+
+        Cursor.SetCursor(noHauntCursor, cursorPoint, cursorMode);
     }
 
     public void HideIcon()
     {
-        icon.enabled = false;
+        //icon.enabled = false;
+
+        Cursor.SetCursor(normalCursor, cursorPoint, cursorMode);
 
     }
 }
