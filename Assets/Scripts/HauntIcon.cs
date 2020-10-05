c6ee3d212b6ceeed6c9ae1f8bc902b389dcea783
Isaiah Mann <isaiahbmann@gmail.com>
2020-10-04 21:43:46 -0700

    Add haunt cursor

---
+++ Assets/Scripts/HauntIcon.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class HauntIcon : MonoBehaviour
+{
+    [SerializeField]
+    Sprite hauntIcon;
+
+    [SerializeField]
+    Sprite hauntDisabledIcon;
+
+    SpriteRenderer icon;
+
+    private void Awake()
+    {
+        icon = GetComponent<SpriteRenderer>();
+        icon.enabled = false;
+    }
+
+    public void ShowHauntEnabledIcon()
+    {
+        icon.enabled = true;
+        icon.sprite = hauntIcon;
+    }
+
+    public void ShowHauntDisabledIcon()
+    {
+        icon.enabled = true;
+        icon.sprite = hauntDisabledIcon;
+    }
+
+    public void HideIcon()
+    {
+        icon.enabled = false;
+
+    }
+}
